d6a295bf5b3a128d684b0c1b2f4376786a7a49f6
Rejwan321 <rejwan.azam.mondal321@gmail.com>
2025-06-06 16:03:33 +0530

    Problems occured

--- src/main/resources/templates/scripts/Script.js
+++ src/main/resources/templates/scripts/Script.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+const signUp = document.getElementById('signup');
+const login = document.getElementById('login');
+const container = document.getElementById('container');
 
-    const signUp = document.getElementById('signup');
-    const login = document.getElementById('login');
-    const container = document.getElementById('container');
-
-    signUp.addEventListener('click', () => {
-        container.classList.add('right-panel-active');
-    })
-    login.addEventListener('click', () => {
-        container.classList.remove('right-panel-active');
-    });
+signUp.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+})
+login.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
 });
