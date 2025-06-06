62d9029f5967b9ce3cbdbfc9eeb9509595d6596b
Rejwan321 <rejwan.azam.mondal321@gmail.com>
2025-06-06 11:36:19 +0530

    Commit done

---
+++ src/main/java/com/reader/Novel/Reader/NovelReaderApplication.java
@@ -0,0 +1,13 @@
+package com.reader.Novel.Reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelReaderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/reader/Novel/Reader/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.reader.Novel.Reader.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String Home() {
+        return "home";
+    }
+}
