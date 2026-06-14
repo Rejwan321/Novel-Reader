4aa7e3df047f4b53ede903a8b4be31283ea92ac7
Partner Killer <partnerkiller.zerotwo@gmail.com>
2026-06-14 18:09:57 +0530

    feat: add chapter comments and optimize reader layout vertical centering

---
+++ src/main/java/com/reader/Novel/Reader/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.reader.Novel.Reader.repository;
+
+import com.reader.Novel.Reader.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByChapterIdOrderByCreatedAtAsc(Long chapterId);
+}
