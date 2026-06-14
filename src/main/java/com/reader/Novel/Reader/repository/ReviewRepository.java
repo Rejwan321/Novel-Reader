6badb4b9fdeb1735d53c964790edd3e9bd5699a7
Partner Killer <partnerkiller.zerotwo@gmail.com>
2026-06-14 13:15:12 +0530

    feat: implement user reviews submission modal and admin review management panel

---
+++ src/main/java/com/reader/Novel/Reader/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.reader.Novel.Reader.repository;
+
+import com.reader.Novel.Reader.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
