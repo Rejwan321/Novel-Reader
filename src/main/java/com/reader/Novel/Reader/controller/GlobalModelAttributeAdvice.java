80b129306a7a2871aec69dec20ab71898ec5fc49
Partner Killer <partnerkiller.zerotwo@gmail.com>
2026-06-17 19:26:52 +0530

    Add Google OAuth client secret and admin config upload, fix Google Sign-in flow

--- src/main/java/com/reader/Novel/Reader/controller/GlobalModelAttributeAdvice.java
+++ src/main/java/com/reader/Novel/Reader/controller/GlobalModelAttributeAdvice.java
@@ -28,32 +28,4 @@ public class GlobalModelAttributeAdvice {
                 .map(com.reader.Novel.Reader.model.SystemSetting::getSettingValue)
                 .orElse("your-google-client-id");
     }
-
-    @ModelAttribute("facebookAppId")
-    public String addFacebookAppId() {
-        return systemSettingRepository.findById("facebook.app_id")
-                .map(com.reader.Novel.Reader.model.SystemSetting::getSettingValue)
-                .orElse("");
-    }
-
-    @ModelAttribute("googleAuthEnabled")
-    public boolean isGoogleAuthEnabled() {
-        return systemSettingRepository.findById("google.auth.enabled")
-                .map(s -> "true".equalsIgnoreCase(s.getSettingValue()))
-                .orElse(true); // Default to true
-    }
-
-    @ModelAttribute("facebookAuthEnabled")
-    public boolean isFacebookAuthEnabled() {
-        return systemSettingRepository.findById("facebook.auth.enabled")
-                .map(s -> "true".equalsIgnoreCase(s.getSettingValue()))
-                .orElse(false); // Default to false
-    }
-
-    @ModelAttribute("xAuthEnabled")
-    public boolean isXAuthEnabled() {
-        return systemSettingRepository.findById("x.auth.enabled")
-                .map(s -> "true".equalsIgnoreCase(s.getSettingValue()))
-                .orElse(false); // Default to false
-    }
 }
